831f33850d5bd559cb591864a6654400a18570bf
jelliotartz <jelliotartz@gmail.com>
2017-09-28 21:45:17 -0500

    Add spec, refactor to SFC.

--- app/pages/admin.jsx
+++ app/pages/admin.jsx
@@ -3,95 +3,85 @@ import counterpart from 'counterpart';
 import Translate from 'react-translate-component';
 import { IndexLink, Link } from 'react-router';
 import { Helmet } from 'react-helmet';
-import ChangeListener from '../components/change-listener';
 
-counterpart.registerTranslations('en', {
-  userAdminPage: {
-    header: 'Admin',
-    nav: {
-      createAdmin: 'Manage Users',
-      projectStatus: 'Set Project Status',
-      grantbot: 'Grantbot',
-      organizationStatus: 'Set Organization Status'
-    }
+const AdminPage = ({ user, children }) => {
+  if (!user) {
+    return (
+      <div className="content-container">
+        <p>You are not signed in</p>
+      </div>
+    );
   }
-});
 
-// class AdminPage extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-
-//   }
-// }
-
-const AdminPage = (props) => {
-// class AdminPage extends React.Component {
-// class AdminPageWrapper extends React.Component {
-
-  {props.user && props.user.admin &&
-    (<ChangeListener target={props.user}>
-        <section className="admin-page-content">
-          <Helmet title={counterpart('userAdminPage.header')} />
-          <div className="secondary-page admin-page">
-            <h2><Translate content="userAdminPage.header" /></h2>
-            <div className="admin-content">
-              <aside className="secondary-page-side-bar admin-side-bar">
-                <nav>
-                  <IndexLink
-                    to="/admin"
-                    type="button"
-                    className="secret-button admin-button"
-                    activeClassName="active"
-                  >
-                    <Translate content="userAdminPage.nav.createAdmin" />
-                  </IndexLink>
-                  <Link
-                    to="/admin/project_status"
-                    type="button"
-                    className="secret-button admin-button"
-                    activeClassName="active"
-                  >
-                    <Translate content="userAdminPage.nav.projectStatus" />
-                  </Link>
-                  <Link
-                    to="/admin/grantbot"
-                    type="button"
-                    className="secret-button admin-button"
-                    activeClassName="active"
-                  >
-                    <Translate content="userAdminPage.nav.grantbot" />
-                  </Link>
-                  <Link
-                    to="/admin/organization-status"
-                    type="button"
-                    className="secret-button admin-button"
-                    activeClassName="active"
-                  >
-                    <Translate content="userAdminPage.nav.organizationStatus" />
-                  </Link>
-                </nav>
-              </aside>
-              <section className="admin-tab-content">
-                {React.cloneElement(props.children, props)}
-              </section>
-            </div>
+  if (user.admin) {
+    return (
+      <section className="admin-page-content">
+        <Helmet title={counterpart('userAdminPage.header')} />
+        <div className="secondary-page admin-page">
+          <h2><Translate content="userAdminPage.header" /></h2>
+          <div className="admin-content">
+            <aside className="secondary-page-side-bar admin-side-bar">
+              <nav>
+                <IndexLink
+                  to="/admin"
+                  type="button"
+                  className="secret-button admin-button"
+                  activeClassName="active"
+                >
+                  <Translate content="userAdminPage.nav.createAdmin" />
+                </IndexLink>
+                <Link
+                  to="/admin/project_status"
+                  type="button"
+                  className="secret-button admin-button"
+                  activeClassName="active"
+                >
+                  <Translate content="userAdminPage.nav.projectStatus" />
+                </Link>
+                <Link
+                  to="/admin/grantbot"
+                  type="button"
+                  className="secret-button admin-button"
+                  activeClassName="active"
+                >
+                  <Translate content="userAdminPage.nav.grantbot" />
+                </Link>
+                <Link
+                  to="/admin/organization-status"
+                  type="button"
+                  className="secret-button admin-button"
+                  activeClassName="active"
+                >
+                  <Translate content="userAdminPage.nav.organizationStatus" />
+                </Link>
+              </nav>
+            </aside>
+            <section className="admin-tab-content">
+              {React.cloneElement(children, { user, children })}
+            </section>
           </div>
-        </section>
-    </ChangeListener>)}
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <div className="content-container">
+        <p>You are not an administrator</p>
+      </div>
+    );
+  }
+};
 
-  {props.user &&
-    (<div className="content-container">
-      <p>You are not an administrator</p>
-    </div>)}
+AdminPage.defaultProps = {
+  children: null,
+  user: null
+};
 
-  (<div className="content-container">
-      <p>You’re not signed in.</p>
-    </div>);
-
-}
+AdminPage.propTypes = {
+  children: React.PropTypes.node,
+  user: React.PropTypes.shape({
+    admin: React.PropTypes.bool
+  })
+};
 
 export default AdminPage;
-
